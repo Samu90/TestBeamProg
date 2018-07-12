b472ebf89769b6af9a0fafe409f9e9f6f96191c9
Samu90 <torelli.1654130@studenti.uniroma1.it>
2018-07-12 17:31:09 +0200

    commento

--- plotWF_graph.C
+++ plotWF_graph.C
@@ -12,31 +12,28 @@ void plotWF_graph(const char * filename){
   TFile *  file= TFile::Open(filename);
   TTree * WFTree = (TTree*)file->Get("wf");
   TTree * digiTree = (TTree*)file->Get("digi");
-  Float_t amp_max[54],time[54];
-  // Float_t a_r[13551],a_l[13551],deltat[13551];
-  int n_timetypes;
-  int j,k;
+
+  Float_t amp_max[54];
+  int k;
+  
   TCanvas* wf_c =new TCanvas("wf","Plot wf",1000,650);
-  TH1F *hr_amp =new TH1F("hr_amp","histos_ampr",100,0.0,400);
-  TH1F *hl_amp =new TH1F("hl_amp","histos_ampl",100,0.0,400);
+  TH1F *hr_amp =new TH1F("hr_amp","histos_ampr",100,0.0,1000);
+  TH1F *hl_amp =new TH1F("hl_amp","histos_ampl",100,0.0,1000);
+
   digiTree->SetBranchAddress("amp_max",&amp_max);
-  digiTree->SetBranchAddress("time",&time);
+  
 
 
   
   for(k=0;k<digiTree->GetEntries();k++){
-    amp_max[3]=0;
-    amp_max[4]=0;
    
-    // if (k%100==0) cout<<k<<endl;
+    if (k%100==0) cout<<k<<endl;
     digiTree->GetEntry(k);
-    cout << k <<"           " << amp_max[3]<< "         " << amp_max[4]<< endl;
+    
     
     hr_amp->Fill(amp_max[3]);
     hl_amp->Fill(amp_max[4]);
-    //    a_r[k]=amp_max[3];
-    // a_l[k]=amp_max[4];
-    //  deltat[k]= time[3]-time[4];
+
   }//chiudo for k   
   
     
@@ -45,10 +42,6 @@ void plotWF_graph(const char * filename){
   hl_amp->SetLineColor(kRed);
   hr_amp->DrawNormalized();
   hl_amp->DrawNormalized("same");
-    // TGraph* graph_r = new TGraph(digiTree->GetEntries(),delta_t,a_r);
-    // TGraph* graph_l = new TGraph(digiTree->GetEntries(),delta_t,a_l);
-    //    graph_r->Draw();
-    // graph_l->Draw("same");
   cout << "here!"<<endl;
 }
 
