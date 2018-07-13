36325beabebdb85a20f41fa6c892f08c94138af7
Samu90 <torelli.1654130@studenti.uniroma1.it>
2018-07-13 16:34:11 +0200

    rinormalizzazione x corretta

--- plotWF_graph.C
+++ plotWF_graph.C
@@ -1,9 +1,3 @@
-#include <iostream>
-#include <TH1.h>
-#include <TCanvas.h>
-#include <TTree.h>
-#include <TFile.h>
-#include <TMath.h>
 
 using namespace std;
 void plotWF_graph(const char * filename){
@@ -15,7 +9,7 @@ void plotWF_graph(const char * filename){
 
   Float_t amp_max[54];
   int k;
-  Double_t max;
+  Double_t max=0;
   
   TCanvas* wf_c =new TCanvas("wf","Plot wf",1200,550);
   wf_c->Divide(2,1);
@@ -23,24 +17,29 @@ void plotWF_graph(const char * filename){
   TH1F *hl_amp =new TH1F("hl_amp","histos_ampl",500,0.0,1);
 
   digiTree->SetBranchAddress("amp_max",&amp_max);
-  max=digiTree->GetMaximum("amp_max");
+  
+  for(k=0;k<digiTree->GetEntries();k++){
+    digiTree->GetEntry(k);
+    if(amp_max[3]>max) {max=amp_max[3];}
+    if(amp_max[4]>max) {max=amp_max[4];}
+  }//chiudo for k
   
   for(k=0;k<digiTree->GetEntries();k++){
     if (k%100==0) cout<<k<<endl;
     digiTree->GetEntry(k);
-
+    
     hr_amp->Fill(amp_max[3]/max);
     hl_amp->Fill(amp_max[4]/max);
   }//chiudo for k   
   
   hl_amp->SetLineColor(kRed);
   
-  
+  cout<< max << endl;
   wf_c->cd(1)->SetLogy();
   hr_amp->DrawNormalized();
   wf_c->cd(2)->SetLogy();
   hl_amp->DrawNormalized();
-
+  
   
   // wf_c->cd(2)->SetLogy();
   //hr_amp->Draw();
