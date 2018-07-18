39103d68a9effd6719ae44c911a1be239cb45624
Samu90 <torelli.1654130@studenti.uniroma1.it>
2018-07-18 17:42:38 +0200

    parametri fit per 1.3,4.1 ok

--- plotWF_tamp.C
+++ plotWF_tamp.C
@@ -22,14 +22,14 @@ void plotWF_tamp(const char * filename){
   rxmax=0.5;
 
  
-  const Int_t  nbinx=200,nbiny=400;
+  const Int_t  nbinx=100, nbiny=500;
 
-  rymin_l=5;
-  rymax_l=18;
-  rymin_r=7;
-  rymax_r=14;
-  tymin=6;
-  tymax=17;
+  rymin_l=12;
+  rymax_l=25;
+  rymin_r=12;
+  rymax_r=20;
+  tymin=13;
+  tymax=20;
  
 
 
@@ -142,22 +142,26 @@ void plotWF_tamp(const char * filename){
   TGraphErrors* graph_l=new TGraphErrors(nbinx-1,x_l,y_l,0,rmsy_l);
   TGraphErrors* graph_t=new TGraphErrors(nbinx-1,xt,yt,0,rmsyt);
 
-  TF1* hyp_r = new TF1("hyp_r","[0] - [1]/(x**[3]-[2])",rxmin,rxmax);
-  TF1* hyp_l = new TF1("hyp_l","[0] - [1]/(x**[3]-[2])",rxmin,rxmax);
-  TF1* hyp_t = new TF1("hyp_t","[0] + [1]*x",-0.5,1);
+  TF1* hyp_r = new TF1("hyp_r","[0] - [1]/(x**[3]-[2])",0,0.4);
+  TF1* hyp_l = new TF1("hyp_l","[0] - [1]/(x**[3]-[2])",0.0,0.45);
+  TF1* hyp_t = new TF1("hyp_t","[0] + [1]*x",-0.1,1);
   //  hyp_r->SetParameter(0,10);
   // hyp_l->SetParameter(0,10);
 
-  /*hyp_r->SetParameter(0,-50);
-  hyp_r->SetParameter(0,-70);
-  hyp_r->SetParameter(0,-0.2);
-  hyp_r->SetParameter(0,0.042);
+  
+  hyp_r->SetParameter(0,1e-3);
+  hyp_r->SetParameter(1,-6);
+  hyp_r->SetParameter(2,-3e-1);
+  hyp_r->SetParameter(3,1.3);
 
-  hyp_l->SetParameter(0,-50);
-  hyp_l->SetParameter(0,-70);
-  hyp_l->SetParameter(0,-0.2);
-  hyp_l->SetParameter(0,0.042);
-  */
+  hyp_l->SetParameter(0, -6e1);
+  hyp_l->SetParameter(1, -2e1);
+  hyp_l->SetParameter(2, 7e-1);
+  hyp_l->SetParameter(3, 2e-2);
+  
+  
+  
+  
 
 
   // hyp_r->SetParLimits(0,1,8);
@@ -168,6 +172,8 @@ void plotWF_tamp(const char * filename){
   wf_c->Divide(3,1);
   
   wf_c->cd(1);
+  h2_l->SetXTitle("max_amp left");
+  h2_l->SetYTitle("t_l - t_MCP (ns)");
   h2_l->Draw("COLZ");
   graph_l->Fit("hyp_l","R");
   graph_l->SetMarkerStyle(8);
@@ -176,6 +182,8 @@ void plotWF_tamp(const char * filename){
   
   
   wf_c->cd(2);
+  h2_r->SetXTitle("max_amp right");
+  h2_r->SetYTitle("t_r - t_MCP (ns)");
   h2_r->Draw("COLZ");
   graph_r->Fit("hyp_r","R");
   graph_r->SetMarkerStyle(8);
@@ -183,6 +191,8 @@ void plotWF_tamp(const char * filename){
   graph_r->Draw("P");
 
   wf_c->cd(3);
+  h2_t->SetXTitle("t_left-t_right (ns)");
+  h2_t->SetYTitle("t_l - t_MCP (ns)");
   h2_t->Draw("COLZ");
   graph_t->Fit("hyp_t","R");
   graph_t->SetMarkerStyle(8);
@@ -190,7 +200,7 @@ void plotWF_tamp(const char * filename){
   graph_t->Draw("P");
   
   
-  
+  wf_c->SaveAs("plot.pdf");
   
   
 }
