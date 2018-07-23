2fa9b499f991765759eebfa9cde25866d2d5a7aa
Samu90 <torelli.1654130@studenti.uniroma1.it>
2018-07-23 18:34:02 +0200

    titoli

--- plotHisto.C
+++ plotHisto.C
@@ -12,20 +12,27 @@ void plotHisto(const char * filename1,const char * filename2){
 
   TCanvas* c1= new TCanvas("mycanvas","",1000,500);
   c1->Divide(2,1);
+
   
   c1->cd(1)->SetLogy();
-  
-  hl1->SetLineColor(1);
+  TLegend* l1=new TLegend(0.1,0.7,0.48,0.9);
+  l1->SetHeader("Configuration","C");
+  l1->AddEntry(hl1,"Conf1.3");
+  l1->AddEntry(hl2,"Conf4.1");
+  hl1->SetLineColor(4);
   hl1->DrawNormalized();
-
   hl2->SetLineColor(2);
   hl2->DrawNormalized("same");
-  
+  l1->Draw();
   
   c1->cd(2)->SetLogy();
-  hr1->SetLineColor(3);
+  TLegend* l2=new TLegend(0.1,0.7,0.48,0.9);
+  l2->SetHeader("Configuration","C");
+  l2->AddEntry(hr1,"Conf1.3");
+  l2->AddEntry(hr2,"Conf4.1");
+  hr1->SetLineColor(4);
   hr1->DrawNormalized();
-  hr2->SetLineColor(4);
+  hr2->SetLineColor(2);
   hr2->DrawNormalized("same");
-  
+  l2->Draw();
 }
