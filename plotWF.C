4bf5fb819b68aeb91c36a3ebdf66be924223b1a2
Raffaella07 <raffaella.tram@gmail.com>
2018-08-27 12:21:28 +0200

    plotWF start time check

--- plotWF.C
+++ plotWF.C
@@ -4,17 +4,20 @@ void plotWF(const char * filename,int i){
   
   TFile *  file= new TFile(filename);
   TTree * WFTree = (TTree*)file->Get("wf");
+  TTree * digiTree = (TTree*)file->Get("digi");
  
-  Float_t tot_WF[6144],wf_time[6144];
-  Float_t ch[6][1024], time[1024];
+  Float_t tot_WF[6144],wf_time[6144],digi_time[54];
+  Float_t ch[6][1024], time[1024],startime[2];
   int wf_sample,step[6144];
-  int j,k;
+  int j,k,LED300;
 
   
   WFTree->SetBranchAddress("WF_time",&wf_time);
   WFTree->SetBranchAddress("WF_val",&tot_WF);
   // WFTree->SetBranchAddress("Instance",&step);
   WFTree->SetBranchAddress("WF_samples",&wf_sample);
+  digiTree->SetBranchAddress("time",&digi_time);
+  digiTree->SetBranchAddress("LED300",&LED300);
   int p;
   p=i;
   
@@ -23,6 +26,7 @@ void plotWF(const char * filename,int i){
     if (i==0) p=k;
     cout<<p<<endl;
     WFTree->GetEntry(p);
+    digiTree->GetEntry(p);
     
     for(j=0;j<wf_sample;j++){
       step[j]= j;
@@ -30,8 +34,11 @@ void plotWF(const char * filename,int i){
       if(j<1024) time[j]= wf_time[j];
     }//chiudo for j
 
+    startime[0]=digi_time[1+LED300];
+    startime[1]=digi_time[2+LED300];
     TCanvas* wf_c =new TCanvas("wf","Plot wf",1000,650);
     TGraph* graph[6];
+    TLine* start[2];
     wf_c->Divide(3,2);
     
     for (j=0;j<6;j++){  
@@ -42,6 +49,16 @@ void plotWF(const char * filename,int i){
       //graph[j]->GetYaxis()->SetTitleSize(10);
       graph[j]->GetYaxis()->SetTitle("amplitude [mV]");
       graph[j]->Draw();
+      if(j==1){
+	start[0]=new TLine(startime[0],0,startime[0],500);
+	start[0]->SetLineColor(kRed);
+	start[0]->Draw("same");
+      }
+      if(j==2){
+	start[1]=new TLine(startime[1],0,startime[1],500);
+	start[1]->SetLineColor(kRed);
+	start[1]->Draw("same");
+      }
     }//chiudo for j
     
     
